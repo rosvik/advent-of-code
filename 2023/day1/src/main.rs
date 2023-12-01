ac7b818518d3879b8e25df7d50a227ba899490cd
Johannes Røsvik <j.rosvik@gmail.com>
2023-12-01 14:54:39 +0100

    refactor

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -9,12 +9,17 @@ fn main() {
             let line = line.unwrap();
 
             let number_chars = get_number_chars(&line);
+
             let first = number_chars.chars().next().unwrap();
             let last = number_chars.chars().last().unwrap();
 
             let first_last_num_string = format!("{}{}", first, last);
             let first_last_num: u32 = first_last_num_string.parse().unwrap();
 
+            println!(
+                "line: {}, numbers: {} -> {}",
+                line, number_chars, first_last_num_string
+            );
             sum += first_last_num;
         }
         println!("SUM: {}", sum);
@@ -22,28 +27,25 @@ fn main() {
 }
 
 fn get_number_chars(line: &str) -> String {
-    let mut numbers: String = String::new();
+    let mut number_chars: String = String::new();
     let num_words = [
         "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
     ];
 
     for (i, ch) in line.chars().enumerate() {
         if ch.is_numeric() {
-            numbers.push(ch);
+            number_chars.push(ch);
         } else if ch.is_alphabetic() {
             let rest_of_line = line[i..].to_string();
             for (i, num_word) in num_words.iter().enumerate() {
                 if rest_of_line.starts_with(num_word) {
-                    numbers.push_str(&i.to_string());
+                    number_chars.push_str(&i.to_string());
                     break;
                 }
             }
         }
     }
-
-    println!("line: {}, numbers: {}", line, numbers);
-
-    numbers
+    number_chars
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
