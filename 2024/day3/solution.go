a66ce53e66d3b34354719a886df229daad0aeeba
Johannes Røsvik <j.rosvik@gmail.com>
2024-12-03 16:52:08 +0100

    solve day 3 part 2

--- 2024/day3/solution.go
+++ 2024/day3/solution.go
@@ -14,6 +14,15 @@ func main() {
 		return
 	}
 
+	// Sanitize the content until no more "don't()" or "do()" are found
+	for true {
+		result := sanitizeOnce(content)
+		if result == nil {
+			break
+		}
+		content = result
+	}
+
 	re := regexp.MustCompile(`mul\((\d{0,3}),(\d{0,3})\)`)
 	matches := re.FindAllStringSubmatch(string(content), -1)
 
@@ -26,4 +35,30 @@ func main() {
 	}
 
 	fmt.Println(total)
+}
+
+func sanitizeOnce(content []byte) []byte {
+	re := regexp.MustCompile(`don't\(\)`)
+	// Index of first "don't()"
+	dontIndex := re.FindIndex(content)
+
+	// If no match, return nil
+	if dontIndex == nil {
+		return nil
+	}
+
+	re = regexp.MustCompile(`do\(\)`)
+	// Relative index of next "do()"
+	doIndex := re.FindIndex(content[dontIndex[1]:])
+
+	// If no match, remove the rest of the content
+	if doIndex == nil {
+		return content[dontIndex[1]:]
+	}
+
+	startIndex := dontIndex[0]
+	endIndex := dontIndex[1] + doIndex[1]
+
+	// Remove the content between the two
+	return append(content[:startIndex], content[endIndex:]...)
 }
